2491d7596aab1d5642bdf693b2b2c8714a1cd158
Postal2007 <evanpostal@gmail.com>
2025-12-10 16:00:02 -0500

    Status = Planned filter

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
             `;
 
             tableBody.appendChild(row);*/
-            if(project.id == 2){
+            if(project.Status == "Planned"){
                 const row = tableBody.insertRow();
                 row.insertCell().textContent = project.ProjectID;
                 row.insertCell().textContent = Project.Province;
