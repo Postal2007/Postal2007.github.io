931962bf4b0735e67720726f244b8f9fef10115e
Postal2007 <evanpostal@gmail.com>
2025-12-10 12:04:02 -0500

    test2

--- script.js
+++ script.js
@@ -26,10 +26,10 @@ document.addEventListener('DOMContentLoaded', () => {
             const row = tableBody.insertRow();
             //row.insertCell().textContent = user.id;
             row.insertCell().textContent = user.username;
-            row.insertCell().textContent = user.name;
-            row.insertCell().textContent = user.email;
-            row.insertCell().textContent = user.company.name;
-            row.insertCell().textContent = user.phone;
+            //row.insertCell().textContent = user.name;
+            //row.insertCell().textContent = user.email;
+            //row.insertCell().textContent = user.company.name;
+            //row.insertCell().textContent = user.phone;
         });
 
     })
