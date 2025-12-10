572d1f5cd5dfbc85d840d0bf309caf5bf414383e
Postal2007 <evanpostal@gmail.com>
2025-12-09 20:55:23 -0500

    Add data fetching and table population functionality
    Fetches user data from a mock API and populates a table.

---
+++ script.js
@@ -0,0 +1,36 @@
+// Fetch data from a mock API
+
+fetch('https://jsonplaceholder.typicode.com/users')
+
+.then(response => response.json())
+
+.then(data => {
+
+const tableBody = document.getElementById('data-table');
+
+data.forEach(user => {
+
+const row = document.createElement('tr');
+
+row.innerHTML = `
+
+<td>${user.id}</td>
+
+<td>${user.username}</td>
+
+<td>${user.name}</td>
+
+<td><a href="mailto:${user.email}">${user.email}</a></td>
+
+<td>${user.company.name}${user.company.catchPhrase}</td>
+
+<td>${user.phone}</td>
+`;
+
+tableBody.appendChild(row);
+
+});
+
+})
+
+.catch(error => console.error('Error fetching data:', error));
